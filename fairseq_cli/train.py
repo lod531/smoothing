905e0d41580d9145e89208847cc1d7d414c8b203
Andrius Buinovskij <andriusb@eu-login-27.euler.ethz.ch>
2022-02-03 13:47:12 +0100

    more kl stuff

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -172,7 +172,7 @@ def main(cfg: FairseqConfig) -> None:
     train_meter = meters.StopwatchMeter()
     train_meter.start()
 
-    epoch_count = 0
+    epoch_count = 1
     eps = 0.0001
     while epoch_itr.next_epoch_idx <= max_epoch:
         #probs = model.softmax(model.weights)
@@ -194,6 +194,7 @@ def main(cfg: FairseqConfig) -> None:
 
 
 
+        epoch_count
         if lr <= cfg.optimization.stop_min_lr:
             logger.info(
                 f"stopping training because current learning rate ({lr}) is smaller "
@@ -217,6 +218,15 @@ def main(cfg: FairseqConfig) -> None:
             # don't cache epoch iterators for sharded datasets
             disable_iterator_cache=task.has_sharded_data("train"),
         )
+
+        avg_uniform_KL = (criterion.KL_div_uniform/criterion.KL_n_terms)
+        avg_unigram_KL = (criterion.KL_div_unigram/criterion.KL_n_terms)
+        print("KL Stats: Epoch " + str(epoch_count) + " Divergences: " + "Uniform: " + str(avg_uniform_KL) + " Unigram: " + str(avg_unigram_KL))
+        criterion.KL_div_uniform = 0
+        criterion.KL_div_unigram = 0
+        criterion.KL_n_terms = 0
+        epoch_count += 1
+
     train_meter.stop()
     logger.info("done training in {:.1f} seconds".format(train_meter.sum))
 
